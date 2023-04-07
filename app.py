d2a664ae9bb50c12c080a943149ac33671bc6fab
Rahul Gore <rahulgore575@gmail.com>
2023-04-07 19:24:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/sequence", methods=["POST"])
+def sequence():
+    num = float(request.form.get("number"))
+    today = datetime.date.today()
+
+    round_num = int(num // 10 * 10)
+    percent = round_num * 0.02
+    result = round(num + percent + 0.4, 2)
+
+    sequence = [(today, num), (today + datetime.timedelta(days=1), result)]
+    for i in range(2, 366):
+        if i % 2 == 0:
+            percent = round((sequence[i-1][1] // 10 * 10) * 0.02, 2)
+            result = round(sequence[i-1][1] + percent + 0.2, 2)
+        else:
+            percent = round((sequence[i-1][1] // 10 * 10) * 0.02, 2)
+            result = round(sequence[i-1][1] + percent + 0.4, 2)
+        sequence.append((today + datetime.timedelta(days=i), result))
+
+    return render_template("sequence.html", sequence=sequence)
+
+if __name__ == "__main__":
+    app.run(debug=True)
